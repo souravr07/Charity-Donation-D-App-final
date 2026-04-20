e98e882f83782d22981c25480ec3cd8bd4197470
souravr07 <261235773+souravr07@users.noreply.github.com>
2026-04-20 21:17:50 +0100

    Add files via upload

--- sha1.js
+++ sha1.js
@@ -1,11 +1,11 @@
 import { createHash } from 'node:crypto';
-function md5(bytes) {
+function sha1(bytes) {
     if (Array.isArray(bytes)) {
         bytes = Buffer.from(bytes);
     }
     else if (typeof bytes === 'string') {
         bytes = Buffer.from(bytes, 'utf8');
     }
-    return createHash('md5').update(bytes).digest();
+    return createHash('sha1').update(bytes).digest();
 }
-export default md5;
+export default sha1;
