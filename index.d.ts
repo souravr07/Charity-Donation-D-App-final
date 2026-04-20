631949ff27a49492661b9d8eedf5d540b18b645b
souravr07 <261235773+souravr07@users.noreply.github.com>
2026-04-20 21:36:24 +0100

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,50 +1,30 @@
 /**
- *  A **Provider** provides a connection to the blockchain, whch can be
- *  used to query its current state, simulate execution and send transactions
- *  to update the state.
+ *  There are many simple utilities required to interact with
+ *  Ethereum and to simplify the library, without increasing
+ *  the library dependencies for simple functions.
  *
- *  It is one of the most fundamental components of interacting with a
- *  blockchain application, and there are many ways to connect, such as over
- *  HTTP, WebSockets or injected providers such as [MetaMask](link-metamask).
- *
- *  @_section: api/providers:Providers  [about-providers]
+ *  @_section api/utils:Utilities  [about-utils]
  */
-export { AbstractProvider, UnmanagedSubscriber } from "./abstract-provider.js";
-export { AbstractSigner, VoidSigner, } from "./abstract-signer.js";
-export { showThrottleMessage } from "./community.js";
-export { getDefaultProvider } from "./default-provider.js";
-export { EnsResolver, MulticoinProviderPlugin } from "./ens-resolver.js";
-export { Network } from "./network.js";
-export { NonceManager } from "./signer-noncemanager.js";
-export { NetworkPlugin, GasCostPlugin, EnsPlugin, FeeDataNetworkPlugin, FetchUrlFeeDataNetworkPlugin, } from "./plugins-network.js";
-export { Block, FeeData, Log, TransactionReceipt, TransactionResponse, copyRequest, } from "./provider.js";
-export { FallbackProvider } from "./provider-fallback.js";
-export { JsonRpcApiProvider, JsonRpcProvider, JsonRpcSigner } from "./provider-jsonrpc.js";
-export { BrowserProvider } from "./provider-browser.js";
-export { AlchemyProvider } from "./provider-alchemy.js";
-export { BlockscoutProvider } from "./provider-blockscout.js";
-export { AnkrProvider } from "./provider-ankr.js";
-export { CloudflareProvider } from "./provider-cloudflare.js";
-export { ChainstackProvider } from "./provider-chainstack.js";
-export { EtherscanProvider, EtherscanPlugin } from "./provider-etherscan.js";
-export { InfuraProvider, InfuraWebSocketProvider } from "./provider-infura.js";
-export { PocketProvider } from "./provider-pocket.js";
-export { QuickNodeProvider } from "./provider-quicknode.js";
-import { IpcSocketProvider } from "./provider-ipcsocket.js";
-export { IpcSocketProvider };
-export { SocketProvider } from "./provider-socket.js";
-export { WebSocketProvider } from "./provider-websocket.js";
-export { SocketSubscriber, SocketBlockSubscriber, SocketPendingSubscriber, SocketEventSubscriber } from "./provider-socket.js";
-export type { AbstractProviderOptions, Subscription, Subscriber, AbstractProviderPlugin, PerformActionFilter, PerformActionTransaction, PerformActionRequest, } from "./abstract-provider.js";
-export type { ContractRunner } from "./contracts.js";
-export type { BlockParams, LogParams, TransactionReceiptParams, TransactionResponseParams, } from "./formatting.js";
-export type { CommunityResourcable } from "./community.js";
-export type { Networkish } from "./network.js";
-export type { GasCostParameters } from "./plugins-network.js";
-export type { BlockTag, TransactionRequest, PreparedTransactionRequest, EventFilter, Filter, FilterByBlockHash, OrphanFilter, ProviderEvent, TopicFilter, Provider, MinedBlock, MinedTransactionResponse } from "./provider.js";
-export type { BrowserDiscoverOptions, BrowserProviderOptions, DebugEventBrowserProvider, Eip1193Provider, Eip6963ProviderInfo } from "./provider-browser.js";
-export type { FallbackProviderOptions } from "./provider-fallback.js";
-export type { JsonRpcPayload, JsonRpcResult, JsonRpcError, JsonRpcApiProviderOptions, JsonRpcTransactionRequest, } from "./provider-jsonrpc.js";
-export type { WebSocketCreator, WebSocketLike } from "./provider-websocket.js";
-export type { Signer } from "./signer.js";
+export { decodeBase58, encodeBase58 } from "./base58.js";
+export { decodeBase64, encodeBase64 } from "./base64.js";
+export { getBytes, getBytesCopy, isHexString, isBytesLike, hexlify, concat, dataLength, dataSlice, stripZerosLeft, zeroPadValue, zeroPadBytes } from "./data.js";
+export { isCallException, isError, assert, assertArgument, assertArgumentCount, assertPrivate, assertNormalize, makeError } from "./errors.js";
+export { EventPayload } from "./events.js";
+export { FetchRequest, FetchResponse, FetchCancelSignal, } from "./fetch.js";
+export { FixedNumber } from "./fixednumber.js";
+export { fromTwos, toTwos, mask, getBigInt, getNumber, getUint, toBigInt, toNumber, toBeHex, toBeArray, toQuantity } from "./maths.js";
+export { resolveProperties, defineProperties } from "./properties.js";
+export { decodeRlp } from "./rlp-decode.js";
+export { encodeRlp } from "./rlp-encode.js";
+export { formatEther, parseEther, formatUnits, parseUnits } from "./units.js";
+export { toUtf8Bytes, toUtf8CodePoints, toUtf8String, Utf8ErrorFuncs, } from "./utf8.js";
+export { uuidV4 } from "./uuid.js";
+export type { BytesLike } from "./data.js";
+export type { ErrorCode, EthersError, UnknownError, NotImplementedError, UnsupportedOperationError, NetworkError, ServerError, TimeoutError, BadDataError, CancelledError, BufferOverrunError, NumericFaultError, InvalidArgumentError, MissingArgumentError, UnexpectedArgumentError, CallExceptionError, InsufficientFundsError, NonceExpiredError, OffchainFaultError, ReplacementUnderpricedError, TransactionReplacedError, UnconfiguredNameError, ActionRejectedError, CallExceptionAction, CallExceptionTransaction, CodedEthersError } from "./errors.js";
+export type { EventEmitterable, Listener } from "./events.js";
+export type { GetUrlResponse, FetchPreflightFunc, FetchProcessFunc, FetchRetryFunc, FetchGatewayFunc, FetchGetUrlFunc } from "./fetch.js";
+export type { FixedFormat } from "./fixednumber.js";
+export type { BigNumberish, Numeric } from "./maths.js";
+export type { RlpStructuredData, RlpStructuredDataish } from "./rlp.js";
+export type { Utf8ErrorFunc, UnicodeNormalizationForm, Utf8ErrorReason } from "./utf8.js";
 //# sourceMappingURL=index.d.ts.map
